81e961b29b1c8428bedef95f730a5d68bc9efb12
alprnshnbs <alperensahinbas@gmail.com>
2026-05-10 04:50:10 +0300

    save

--- Assets/_GameAssets/Scripts/PlayerMovement.cs
+++ Assets/_GameAssets/Scripts/PlayerMovement.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Hareket Ayarlari")]
-    public float moveSpeed = 8f;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [Range(0f, 1f)] 
+    [SerializeField] private float jumpCutMultiplier;
+
+    [Header("Zemin Kontrolu")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private float moveInput;
+    private bool isGrounded;
 
     void Start()
     {
@@ -17,10 +26,32 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Zıplama başlangıcı
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutMultiplier);
+        }
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
