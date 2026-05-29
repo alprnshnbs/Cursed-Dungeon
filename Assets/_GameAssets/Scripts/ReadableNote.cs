70e03d1d00bbd4aa97f854b7a4e0589ccbecbdf1
alprnshnbs <alperensahinbas@gmail.com>
2026-05-29 19:58:05 +0300

    NotePaperAdded

---
+++ Assets/_GameAssets/Scripts/ReadableNote.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using TMPro;
+
+public class ReadableNote : MonoBehaviour
+{
+    [Header("UI Ayarlari")]
+    [SerializeField] private GameObject noteUIPanel;
+    [SerializeField] private TextMeshProUGUI noteTextDisplay;
+
+    [Header("Icerik Ayari")]
+    [TextArea(3, 10)]
+    [SerializeField] private string noteContent;
+
+    private bool isPlayerNear;
+    private bool isNoteOpen;
+
+    void Start()
+    {
+        if (noteUIPanel != null)
+        {
+            noteUIPanel.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            if (isNoteOpen)
+            {
+                CloseNote();
+            }
+            else
+            {
+                OpenNote();
+            }
+        }
+    }
+
+    private void OpenNote()
+    {
+        isNoteOpen = true;
+        noteTextDisplay.text = noteContent;
+        noteUIPanel.SetActive(true);
+    }
+
+    public void CloseNote()
+    {
+        isNoteOpen = false;
+        noteUIPanel.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            CloseNote();
+        }
+    }
+}
